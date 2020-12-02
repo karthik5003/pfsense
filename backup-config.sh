69765cf0a0b4329841fc41d82300a099a69f99fd
karthikg <35952422+karthik5003@users.noreply.github.com>
2020-12-02 21:09:29 +0530

    Update backup-config.sh

--- backup-config.sh
+++ backup-config.sh
@@ -2,18 +2,17 @@
 #
 
 # Description: Backup pfSense config xml file to remote server and remove backups older than X date.
-#SSH Login Without Password Using ssh-keygen & ssh-copy-id to REMOTE_HOST from pfSense
 # Usage: ./backup-config.sh
 #
 
-REMOTE_HOST="centos@192.168.1.102"
-REMOTE_DIR="/home/centos/pfsense"
-#DAYS=30  #Number of days of backups to keep on REMOTE_HOST
+REMOTE_HOST="host.domain.com"
+REMOTE_DIR="/path-to-backup/pfsense"
+DAYS=30  #Number of days of backups to keep on REMOTE_HOST
 
 # Copy pfSense config xml file to remote host
-/usr/bin/scp /cf/conf/config.xml $REMOTE_HOST:$REMOTE_DIR/config-fw.hackerrank.com-`date +%Y%m%d%H%M%S`.xml
+/usr/bin/scp /cf/conf/config.xml $REMOTE_HOST:$REMOTE_DIR/config-$HOST-`date +%Y%m%d%H%M%S`.xml
 
 # Cleanup backed up files on remote host older than x days
-#/usr/bin/ssh -t $REMOTE_HOST "/bin/find $REMOTE_DIR -mtime +$DAYS -exec rm {} \;"
+/usr/bin/ssh -t $REMOTE_HOST "/bin/find $REMOTE_DIR -mtime +$DAYS -exec rm {} \;"
 
 exit 0
